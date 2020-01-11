1870fc4daae5d0daebf992e8c078675ac5f72c61
ebihara.yuki <ebihara.yuki@ricksoft.jp>
2020-01-12 01:39:54 +0900

    Refactoring.

--- audit-log-browser-platform/src/main/java/jp/ricksoft/auditlogbrowser/file/ZipManager.java
+++ audit-log-browser-platform/src/main/java/jp/ricksoft/auditlogbrowser/file/ZipManager.java
@@ -8,11 +8,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
-import org.apache.commons.io.IOUtils;
 
 import com.google.common.io.Files;
 
@@ -33,35 +30,23 @@ public class ZipManager
      */
     public void createZip(File zip, File[] files)
     {
-        ZipOutputStream zos = null;
-        try
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zip))))
         {
-            zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zip)));
             this.createZip(zos, files);
         } catch (IOException e)
         {
             e.printStackTrace();
-        } finally
-        {
-            IOUtils.closeQuietly(zos);
         }
     }
 
+
     private void createZip(ZipOutputStream zos, File[] files) throws IOException
     {
-        InputStream is = null;
-        try
+        for (File file : files)
         {
-            for (File file : files)
-            {
-                ZipEntry entry = new ZipEntry(file.getName());
-                zos.putNextEntry(entry);
-                Files.copy(file, zos);
-            }
-
-        } finally
-        {
-            IOUtils.closeQuietly(is);
+            ZipEntry entry = new ZipEntry(file.getName());
+            zos.putNextEntry(entry);
+            Files.copy(file, zos);
         }
     }
 
@@ -72,7 +57,7 @@ public class ZipManager
      * @return created zip file
      * @throws IOException
      */
-    public File prepareZip(File folder) throws IOException
+    public File prepareZip(File folder)
     {
 
         if (folder.list() == null || folder.list().length < 1)
