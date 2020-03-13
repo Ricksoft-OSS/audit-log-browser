0e440534dc6ff5f0bf71b5c64b0e2336ba1c338e
ebihara.yuki <ebihara.yuki@ricksoft.jp>
2020-03-13 10:36:28 +0900

    license info improvement.

--- audit-log-browser-platform/src/main/java/jp/ricksoft/auditlogbrowser/NodeRef/RepositoryFolderManager.java
+++ audit-log-browser-platform/src/main/java/jp/ricksoft/auditlogbrowser/NodeRef/RepositoryFolderManager.java
@@ -1,10 +1,24 @@
-/*
- * Copyright 2018 Ricksoft Co., Ltd.
- * All rights reserved.
- */
 package jp.ricksoft.auditlogbrowser.NodeRef;
 
-import java.io.File;
+/*-
+ * #%L
+ * Audit Log Browser Platform JAR Module
+ * %%
+ * Copyright (C) 2018 - 2020 Ricksoft Co., Ltd.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.nodelocator.CompanyHomeNodeLocator;
@@ -13,9 +27,11 @@ import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.model.FileFolderService;
 import org.alfresco.service.cmr.repository.NodeRef;
 
+import java.io.File;
+
 
 public class RepositoryFolderManager {
-    
+
     private NodeLocatorService nodeLocatorService;
     private FileFolderService fileFolderService;
 
