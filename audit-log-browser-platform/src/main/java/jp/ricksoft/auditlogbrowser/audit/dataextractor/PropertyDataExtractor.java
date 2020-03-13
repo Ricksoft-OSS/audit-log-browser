1b3aef2a540dd3cf526a6cf51523422e2ee1d6eb
ebihara.yuki <ebihara.yuki@ricksoft.jp>
2020-03-13 10:36:28 +0900

    license info improvement.

--- audit-log-browser-platform/src/main/java/jp/ricksoft/auditlogbrowser/audit/dataextractor/PropertyDataExtractor.java
+++ audit-log-browser-platform/src/main/java/jp/ricksoft/auditlogbrowser/audit/dataextractor/PropertyDataExtractor.java
@@ -1,15 +1,35 @@
 package jp.ricksoft.auditlogbrowser.audit.dataextractor;
 
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+/*-
+ * #%L
+ * Audit Log Browser Platform JAR Module
+ * %%
+ * Copyright (C) 2018 - 2020 Ricksoft Co., Ltd.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 
 import org.alfresco.repo.audit.extractor.AbstractDataExtractor;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.util.PropertyCheck;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PropertyDataExtractor extends AbstractDataExtractor {
 
